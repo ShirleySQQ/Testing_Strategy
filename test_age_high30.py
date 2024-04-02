55514934757c91c42124cd4f98fdf307e56a080d
Shirley_Shi <shirley_shi@epamcom>
2024-04-02 11:15:01 +0800

    add test for filter_age() and test out_tocsv()

--- test_age_high30.py
+++ test_age_high30.py
@@ -2,7 +2,7 @@
 import pyspark.sql.functions as F
 
 import filter_Age_Above30
-from filter_Age_Above30 import output_ageHigh30, out_30_data  # Import the function from main.py file
+from filter_Age_Above30 import output_ageHigh30  # Import the function from main.py file
 from pyspark.sql import SparkSession
 
 spark = filter_Age_Above30.create_spark_session()
@@ -11,6 +11,7 @@ spark = filter_Age_Above30.create_spark_session()
 def test_age_high30():
     # Setup
     output_ageHigh30(spark)
+    out_30_data = 'higher_30.csv'
     # Then
     df_out = spark.read.format("csv").option("delimiter", ",").option("header", True).load(out_30_data)
     assert df_out.filter(F.col('Age') <= 30).count() == 0
